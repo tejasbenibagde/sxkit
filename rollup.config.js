78af68efe804f9dff081ced77992d0bcc6a5d554
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-04-27 15:58:27 +0530

    minor change

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
     format: "es",
     name: "sxkit",
   },
-  external: ["react", "react-dom"],
+  external: ["react"],
   plugins: [typescript({ tsconfig: "tsconfig.json" })],
 });
